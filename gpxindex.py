2a3b1f57486e8232c35381265c5730bd11c65043
Terry Brown <terrynbrown@gmail.com>
2019-08-13 10:59:56 -0500

    Finally works, pool workers must return
    return, not exit, but for that the basic_cancel() was needed.

--- gpxindex.py
+++ gpxindex.py
@@ -1,4 +1,6 @@
 """
+rabbit mq based task distribution, perhaps should be celery instead
+
 gpxindex.py - index may GPS track files
 
 Terry N. Brown terrynbrown@gmail.com Mon Aug 12 19:09:17 CDT 2019
@@ -6,10 +8,12 @@ Terry N. Brown terrynbrown@gmail.com Mon Aug 12 19:09:17 CDT 2019
 
 import argparse
 import json
+import multiprocessing
 import os
 import pika
 import time
 from pathlib import Path
+from uuid import uuid4
 
 QUEUE = 'gpxindex'
 
@@ -24,6 +28,9 @@ def make_parser():
         "--load-queue",
         help="Queue files to index from folder",
         metavar='FOLDER',
+    )
+    parser.add_argument(
+        "--spawn", help="Spawn N workers", metavar='N', type=int
     )
     parser.add_argument(
         "--show-queue", action='store_true', help="Show files queued to index"
@@ -70,7 +77,6 @@ def get_connection_channel(**kwargs):
     )
     channel = connection.channel()
     channel.queue_declare(queue=QUEUE)
-    channel.basic_qos(prefetch_count=1)
     return connection, channel
 
 
@@ -87,7 +93,7 @@ def load_queue(opt):
     for path, dirs, files in os.walk(opt.load_queue):
         for filename in [i for i in files if i.lower().endswith('.gpx')]:
             filepath = Path(path).joinpath(filename)
-            print(filepath)
+            print('+', filepath)
             task = dict(filepath=str(filepath))
             channel.basic_publish(
                 exchange='', routing_key=QUEUE, body=enc(task)
@@ -98,40 +104,21 @@ def load_queue(opt):
     connection.close()
 
 
+def show_item_cb(ch, method, properties, body):
+    print('*', dec(body).get('filepath'))
+
+
 def show_queue(opt):
     connection, channel = get_connection_channel()
-    # https://www.rabbitmq.com/specification.html
-    # basic.reject - "No attempt is made to prevent redelivery to the same
-    # client."
-    put_back = []
-
-    def show_item_cb(ch, method, properties, body, put_back=put_back):
-        body = dec(body)
-        print(body.get('filepath'))
-        put_back.append(body)
-
-    channel.basic_consume(
-        queue=QUEUE, on_message_callback=show_item_cb, auto_ack=True
-    )
-    # channel.start_consuming()
+    channel.basic_consume(queue=QUEUE, on_message_callback=show_item_cb)
     connection.process_data_events(time_limit=0)
-
-    connection, channel = get_connection_channel()
-    for task in put_back:
-        print(task)
-        channel.basic_publish(exchange='', routing_key=QUEUE, body=enc(task))
-
     connection.close()
-
-
-def clear_item_cb(ch, method, properties, body):
-    print(dec(body).get('filepath'))
 
 
 def clear_queue(opt):
     connection, channel = get_connection_channel()
     channel.basic_consume(
-        queue=QUEUE, on_message_callback=clear_item_cb, auto_ack=True
+        queue=QUEUE, on_message_callback=show_item_cb, auto_ack=True
     )
     connection.process_data_events(time_limit=0)
     if opt.exit:
@@ -144,23 +131,43 @@ def clear_queue(opt):
 def proc_item_cb(ch, method, properties, body):
     body = dec(body)
     if body.get('EXIT'):
-        exit()
-    print(body['filepath'])
+        return False
+    print("X:", body['filepath'])
     time.sleep(5)
     ch.basic_ack(delivery_tag=method.delivery_tag)
+    return True
 
 
 def proc_queue(opt):
     connection, channel = get_connection_channel()
-    channel.basic_consume(queue=QUEUE, on_message_callback=proc_item_cb)
+    channel.basic_qos(prefetch_count=1)
+    tag = str(uuid4())
+
+    def cb(ch, method, properties, body, tag=tag):
+        if not proc_item_cb(ch, method, properties, body):
+            print("Cancelling")
+            ch.basic_cancel(tag)
+
+    channel.basic_consume(
+        queue=QUEUE, on_message_callback=cb, consumer_tag=tag
+    )
+    print(tag)
     channel.start_consuming()
     # connection.process_data_events(time_limit=10)
     connection.close()
 
 
+def spawn(opt):
+    p = multiprocessing.Pool(opt.spawn)
+    for i in range(opt.spawn):
+        p.apply_async(proc_queue, (opt,))
+    p.close()
+    p.join()  # doesn't work because
+
+
 def main():
     opt = get_options()
-    for queue_op in 'load_queue', 'show_queue', 'clear_queue':
+    for queue_op in 'load_queue', 'show_queue', 'clear_queue', 'spawn':
         if getattr(opt, queue_op):
             globals()[queue_op](opt)
             break
